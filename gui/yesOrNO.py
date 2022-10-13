70823ded13f63bdc7054453cd65d0097c2e73db4
ExaDataInfra <96202076+ExaDataInfra@users.noreply.github.com>
2022-10-13 20:03:40 -0300

    Create yesOrNO.py
    CI

---
+++ gui/yesOrNO.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.messagebox import askyesno
+
+# create the root window
+root = tk.Tk()
+root.title('Tkinter Yes/No Dialog')
+root.geometry('300x150')
+
+# click event handler
+def confirm():
+    answer = askyesno(title='confirmation',
+                    message='Are you sure that you want to quit?')
+    if answer:
+        root.destroy()
+    else:
+        answer = askyesno(title='confirmation',
+                    message='Tenes que irte!')
+        if answer:
+            root.destroy()
+
+
+ttk.Button(
+    root,
+    text='Ask Yes/No',
+    command=confirm).pack(expand=True)
