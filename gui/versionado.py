a464407b755600dc70886ace062d12234d7bea07
ExaDataInfra <96202076+ExaDataInfra@users.noreply.github.com>
2022-10-13 19:59:14 -0300

    Create versionado.py
    CI

---
+++ gui/versionado.py
@@ -0,0 +1,28 @@
+VSVersionInfo(
+  ffi=FixedFileInfo(
+    filevers=(6, 1, 7601, 17514),
+    prodvers=(6, 1, 7601, 17514),
+    mask=0x3f,
+    flags=0x0,
+    OS=0x40004,
+    fileType=0x1,
+    subtype=0x0,
+    date=(0, 0)
+    ),
+  kids=[
+    StringFileInfo(
+      [
+      StringTable(
+        u'040904B0',
+        [StringStruct(u'CompanyName', u'IFTS'),
+        StringStruct(u'FileDescription', u'Calcula Raiz Cuadrada'),
+        StringStruct(u'FileVersion', u'0.1.1'),
+        StringStruct(u'InternalName', u'cmd'),
+        StringStruct(u'LegalCopyright', u'\xa9 IFTS. All rights reserved.'),
+        StringStruct(u'OriginalFilename', u'betaTest.Exe'),
+        StringStruct(u'ProductName', u'Calculadora de raiz cuadrada'),
+        StringStruct(u'ProductVersion', u'0.0.1')])
+      ]), 
+    VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+  ]
+)
